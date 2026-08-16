a4cb2c317492a6c7491120f657900b7017a45603
Minahil Tariq <minahiltariq392@gmail.com>
2026-08-16 22:54:09 +0500

    Update create_memory_for_llm.py

--- create_memory_for_llm.py
+++ create_memory_for_llm.py
@@ -3,10 +3,6 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
-## Uncomment the following files if you're not using pipenv as your virtual environment manager
-#from dotenv import load_dotenv, find_dotenv
-#load_dotenv(find_dotenv())
-
 
 # Step 1: Load raw PDF(s)
 DATA_PATH="data/"
